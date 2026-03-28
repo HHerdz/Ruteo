2d83530ea790dcb7c012359ac1328c9780d02ae6
santiagoquinones171 <santiagoquinones171@gmail.com>
2026-03-28 09:54:01 -0500

    navbar

--- env_ruteo/frontend/src/app/app.config.ts
+++ env_ruteo/frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(NgbModule)
   ]
 };
--- env_ruteo/frontend/src/app/app.ts
+++ env_ruteo/frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbModule, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
