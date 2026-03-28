82fba8b30232ccaf6ebb37d5ac79594e2a459e97
santiagoquinones171 <santiagoquinones171@gmail.com>
2026-03-28 08:48:14 -0500

    dotenv

--- env_ruteo/database.py
+++ env_ruteo/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
-load_dotenv()
+load_dotenv(Path(__file__).resolve().parent / ".env")
 
 DB_URL = os.getenv("DB_URL")
 
