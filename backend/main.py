6f98a05b0278ef40aef0d3ccf488244acdb3d45f
santiagoquinones171 <santiagoquinones171@gmail.com>
2026-04-08 20:53:58 -0500

    RUTEO-JWT3

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from metodos import consultarApi, authApi          
+from fastapi.responses import JSONResponse
+from metodos import consultarApi, authApi
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    if request.method == "OPTIONS":
+        response = JSONResponse(content={}, status_code=200)
+    else:
+        response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=  False,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
